13a99ddb6e0e29bbc9b8a994194786d77c33faf6
Stefano.In91 <ste.ince91@gmail.com>
2022-11-06 17:39:39 +0100

    adds foreach variations to loops, variable for wanted numbers

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 // Riempie array con 5 numeri casuali
-function generateNumbers(array) {
+function generateNumbers(array, wantedLength) {
   let number;
-  while (array.length < 5) {
+  while (array.length < wantedLength) {
     number = getRandomInt(1, 100);
     if (!array.includes(number)) {
       array.push(number);
@@ -25,21 +25,40 @@ function eraseField(container) {
 // Crea campo con numeri da array (svuota preventivamente)
 function makeField(container, array) {
   eraseField(container);
-  for (let i = 0; i < array.length; i++) {
+  // for (let i = 0; i < array.length; i++) {
+  //   const cellNumber = document.createElement("div");
+  //   cellNumber.innerHTML = array[i];
+  //   container.append(cellNumber);
+  // }
+  array.forEach((element) => {
     const cellNumber = document.createElement("div");
-    cellNumber.innerHTML = array[i];
+    cellNumber.innerHTML = element;
     container.append(cellNumber);
-  }
+  });
 }
 // Controlla che i numeri inseriti siano presenti in quelli dell'array e restituisce numero
 // delle risposte esatte, colora celle a seconda della risposta
 function simonEpilogue(container, array, answerArray) {
   let counter = 0;
-  for (let i = 0; i < array.length; i++) {
+  // for (let i = 0; i < array.length; i++) {
+  //   const cellNumber = document.createElement("div");
+  //   cellNumber.innerHTML = array[i];
+  //   cellNumber.style.borderWidth = `4px`;
+  //   if (answerArray.includes(array[i])) {
+  //     cellNumber.style.color = `darkgreen`;
+  //     cellNumber.style.borderColor = `darkgreen`;
+  //     counter++;
+  //   } else {
+  //     cellNumber.style.color = `darkred`;
+  //     cellNumber.style.borderColor = `darkred`;
+  //   }
+  //   container.append(cellNumber);
+  // }
+  array.forEach((element) => {
     const cellNumber = document.createElement("div");
-    cellNumber.innerHTML = array[i];
+    cellNumber.innerHTML = element;
     cellNumber.style.borderWidth = `4px`;
-    if (answerArray.includes(array[i])) {
+    if (answerArray.includes(element)) {
       cellNumber.style.color = `darkgreen`;
       cellNumber.style.borderColor = `darkgreen`;
       counter++;
@@ -48,18 +67,27 @@ function simonEpilogue(container, array, answerArray) {
       cellNumber.style.borderColor = `darkred`;
     }
     container.append(cellNumber);
-  }
+  });
   return counter;
 }
 // Domanda numeri, chiama funzione per sapere quante risposte esatte sono state date
 // Stampa in HTML il risultato
-function simonQuestion(container, array, answerArray, status) {
+function simonQuestion(container, array, status) {
+  const answerArray = [];
   for (let i = 0; i < array.length; i++) {
     let answer = Number(
       prompt(`Inserisci il ${i + 1}° dei ${array.length} numeri`)
     );
     answerArray.push(answer);
   }
+  // array.forEach((element) => {
+  //   let answer = Number(
+  //     prompt(
+  //       `Inserisci il ${array.indexOf(element)}° dei ${array.length} numeri`
+  //     )
+  //   );
+  //   answerArray.push(answer);
+  // });
 
   let correctAnswer = simonEpilogue(container, array, answerArray);
   if (correctAnswer > (array.length / 5) * 3) {
@@ -86,7 +114,8 @@ generator.addEventListener("submit", function (event) {
   gameStatus.innerHTML = "";
 
   const simonNumbers = [];
-  makeField(board, generateNumbers(simonNumbers));
+  let wantedNumbers = 5;
+  makeField(board, generateNumbers(simonNumbers, wantedNumbers));
   const countdown = 5;
   alert(`hai ${countdown} secondi per memorizzare i numeri`);
 
@@ -94,8 +123,7 @@ generator.addEventListener("submit", function (event) {
     eraseField(board);
   }, countdown * 1000);
 
-  const userAnswers = [];
   setTimeout(function () {
-    simonQuestion(board, simonNumbers, userAnswers, gameStatus);
+    simonQuestion(board, simonNumbers, gameStatus);
   }, countdown * 1050);
 });
